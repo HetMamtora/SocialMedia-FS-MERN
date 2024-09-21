8cbe34e7bf5a7b1a559ffbb8371cf382be14cf12
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-09-21 15:25:27 +0530

    updated

--- social/src/pages/home/Home.jsx
+++ social/src/pages/home/Home.jsx
@@ -1,18 +1,18 @@
 import React, {useEffect} from 'react'
-// import PostSide from '../../components/PostSide/PostSide'
-// import ProfileSide from '../../components/profileSide/ProfileSide'
-// import RightSide from '../../components/RightSide/RightSide'
+import PostSide from '../../components/PostSide/PostSide'
+import ProfileSide from '../../components/ProfileSide/ProfileSide'
+import RightSide from '../../components/RightSide/RightSide'
 import './Home.css'
 import {useNavigate} from 'react-router-dom'
 
 const Home = () => {
 
     const navigate = useNavigate()
-    useEffect(() => {
-        if(!localStorage.getItem('userId')){
-            navigate("/")
-        }
-    },[])
+    // useEffect(() => {
+    //     if(!localStorage.getItem('userId')){
+    //         navigate("/")
+    //     }
+    // },[])
     
     return (
         <div className='Home'>
