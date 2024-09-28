f344152698a9c9389510506b80ca3fd6496e393c
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-09-28 16:05:50 +0530

    updated

--- social/src/components/Posts/Posts.jsx
+++ social/src/components/Posts/Posts.jsx
@@ -1,8 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import './Posts.css'
 //import { PostsData } from '../../Data/PostsData'
+import Post from '../Post/Post'
+import Comment from '../../img/comment.png'
+import Share from '../../img/share.png'
+import Heart from '../../img/like.png'
+import NotLike from '../../img/notlike.png'
+
 
 const Posts = () => {
+
+    const [liked, setLiked] = useState('')
+    const [posts, setPosts] = useState([])
+
+    const fetchPosts = async() => {
+        const response = await fetch('http://localhost:8080/api/posts/fetchAllPosts');
+        const converted = await response.json()
+        setPosts(converted)
+    }
+    useEffect(()=> {
+        fetchPosts()
+    }, [])
+
+    const handleLikes = async(post) => {
+        console.log("Attributes", post);
+
+        const formData = {
+            _id: post._doc._id,
+            userId: localStorage.getItem("userId")
+        }
+
+        const response = await fetch('http://localhost:8080/api/profile/like', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData),
+        });
+        console.log('Like Response', response)
+
+        if(response.ok){
+            const resp = await response.json();
+            console.log("Result:",resp)
+            fetchPosts()
+        }
+    }
+
     return (
         <div>Posts</div>
     )
