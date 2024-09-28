ad1988ad5ba4dd68acbeebf002664b6cb06daa32
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-09-28 20:53:26 +0530

    updated

--- social/src/components/FollowersCard/FollowersCard.jsx
+++ social/src/components/FollowersCard/FollowersCard.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FollowersCard = () => {
+
+    const [users, setUsers] = useState([]);
+    const [unfollow, setUnfollow] = useState([]);
+    const fetchUsers = async() => {
+        const response = await fetch("http://localhost:8080/api/users/fetchUsers");
+        const converted = await response.json()
+        console.log("All Users", converted);
+        setUsers(converted);
+        
+    }
+    useEffect(() => {
+        fetchUsers()
+        setUnfollow(localStorage.getItem("followersList"))
+    }, [])
+
+    const handleFollow = async (followerId) => {
+        const formData = {
+            userId: localStorage
+        }
+    }
     return (
-        <div>FollowersCard</div>
+        <div className='FollowersCard'>
+            <h3>People You May Follow</h3>
+
+            {users.length > 0 ? users.filter(item=>!localStorage.getItem("name").startsWith(item.firstName)).map((follower,id) => {
+                return(
+                    <div className='follower'>
+                        <div>
+                            <img src={follower.img} alt='' className='followerImage' />
+                            <div className='name'>
+                                <span>{follower.firstName}</span>
+                                <span>@{follower.username}</span>
+                            </div>
+                        </div>
+
+                        <button className='button fc-button' onClick={() => handleFollow(follower)}>
+                            {unfollow?.includes(follower._id) ? 'Unfollow' : 'Follow'}
+                        </button>
+                    </div>
+                )
+            }) : null}
+        </div>
     )
 }
 
