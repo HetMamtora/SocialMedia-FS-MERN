7e9038e62a2913efd93ee4972a8dbc8657b69deb
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-09-29 16:14:00 +0530

    Updated

--- social/src/pages/Profile/Profile.jsx
+++ social/src/pages/Profile/Profile.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './Profile.css'
+import ProfileLeft from '../../components/ProfileLeft/ProfileLeft'
+import ProfileCard from '../../components/ProfileCard/ProfileCard'
+import PostSide from '../../components/PostSide/PostSide'
+import RightSide from '../../components/RightSide/RightSide'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
+
+    // const navigate = useNavigate()
+    // useEffect(() => {
+    //     if(!localStorage.getItem("userId")){
+    //         navigate("/")
+    //     }
+    // },[])
+
     return (
-        <div>Profile</div>
+        <div className='Profile'>
+            <ProfileLeft />
+            <div className='Profile-center'>
+                <ProfileCard />
+                <PostSide />
+            </div>
+            <RightSide />
+        </div>
     )
 }
 
