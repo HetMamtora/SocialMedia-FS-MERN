eb2a063f4d0dd0a6a1587cf557109ad86c2d2870
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-10-02 12:09:35 +0530

    updated

--- social/src/components/FollowersCard/FollowersCard.jsx
+++ social/src/components/FollowersCard/FollowersCard.jsx
@@ -1,58 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import './FollowersCard.css'
-const FollowersCard = () => {
 
+const FollowersCard = () => {
     const [users, setUsers] = useState([]);
-    const [unfollow, setUnfollow] = useState([]);
-    const fetchUsers = async() => {
+    const [unfollow, setUnfollow] = useState([])
+    const fetchUsers = async () => {
         const response = await fetch("http://localhost:8080/api/users/fetchUsers");
         const converted = await response.json()
-        console.log("All Users", converted);
-        setUsers(converted);
-        
+        console.log("all users", converted)
+        setUsers(converted)
     }
     useEffect(() => {
         fetchUsers()
         setUnfollow(localStorage.getItem("followersList"))
     }, [])
 
-    const handleFollow = async (followerId) => {
+    const handleFollow = async (follwoerId) => {
         const formData = {
             userId: localStorage.getItem("userId"),
-            followerId: followerId._id
-        }
+            followerId: follwoerId._id
 
+        }
         const response = await fetch('http://localhost:8080/api/profile/follow', {
-            methof: 'POST',
-            headers:{
+            method: 'POST',
+            headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(formData),
         });
-
         const userData = await response.json()
         setUnfollow(userData.followersList)
-        localStorage.setItem("followersList", userData.followersList)
-        console.log("Posting... Info Response", userData);
-        
+        localStorage.setItem("followersList" ,userData.followersList )
+        console.log("Posting..... info response", userData);
     }
     return (
-        <div className='FollowersCard'>
-            <h3>People You May Follow</h3>
+        <div className="FollowersCard">
+            <h3>People you may follow</h3>
 
-            {users.length > 0 ? users.filter(item=>!localStorage.getItem("name").startsWith(item.firstName)).map((follower,id) => {
-                return(
-                    <div className='follower'>
+            {users.length > 0 ? users.filter(item=>!localStorage.getItem("name").startsWith(item.firstName)).map((follower, id) => {
+                return (
+                    <div className="follower">
                         <div>
-                            <img src={follower.img} alt='' className='followerImage' />
-                            <div className='name'>
+                            <img src={follower.img} alt="" className='followerImage' />
+                            <div className="name">
                                 <span>{follower.firstName}</span>
                                 <span>@{follower.username}</span>
                             </div>
                         </div>
-
                         <button className='button fc-button' onClick={() => handleFollow(follower)}>
-                            {unfollow?.includes(follower._id) ? 'Unfollow' : 'Follow'}
+                           {unfollow?.includes(follower._id) ? 'Unfollow' : 'Follow'} 
                         </button>
                     </div>
                 )
